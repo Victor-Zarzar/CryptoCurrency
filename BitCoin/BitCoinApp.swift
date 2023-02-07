e4d1dbf532d708995b54cd3dd16d4b84466f9910
Victor-Zarzar <victorzarzar58@gmail.com>
2023-02-07 12:13:33 -0300

    Initial Commit

---
+++ BitCoin/BitCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitCoinApp.swift
+//  BitCoin
+//
+//  Created by Victor Zarzar on 07/02/23.
+//
+
+import SwiftUI
+
+@main
+struct BitCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
